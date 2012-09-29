9b54a4c6f480607fa344d4c089dbe86881b4591f
Tylor F <tylor@diemetalle.com>
2012-09-29 10:58:45 -0600

    PHP Auth Details

---
+++ rdio-consumer-credentials.php
@@ -0,0 +1,6 @@
+<?php
+# you can get these by signing up for a developer account at:
+# http://developer.rdio.com/
+define('RDIO_CONSUMER_KEY', 'rtdx4d6frpcck2ej8qq6u5cj');
+define('RDIO_CONSUMER_SECRET', 'NxUDrCNuwJ');
+?>
